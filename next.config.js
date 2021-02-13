d1398e76831a62c1724ee41ae7f1bf3e8633a0ec
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-02-13 17:51:01 +0100

    Configure github action for deploy

--- next.config.js
+++ next.config.js
@@ -1,22 +1,57 @@
-module.exports = {
-    async rewrites() {
-        return [
-            {
-                source: "/about",
-                destination: "/",
-            },
-            {
-                source: "/home",
-                destination: "/",
-            },
-            {
-                source: "/formations",
-                destination: "/formations/git-github",
-            },
-            {
-                source: "/formation",
-                destination: "/formations/git-github",
-            },
-        ];
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
+
+module.exports = withPlugins([
+    [optimizedImages, {}],
+
+    {
+        async redirects() {
+            return [
+                {
+                    source: "/about",
+                    destination: "/home",
+                    permanent: false,
+                },
+            ];
+        },
+        async rewrites() {
+            return [
+                {
+                    source: "/home",
+                    destination: "/",
+                },
+                {
+                    source: "/formations",
+                    destination: "/formations/git-github",
+                },
+                {
+                    source: "/formation",
+                    destination: "/formations/git-github",
+                },
+            ];
+        },
+        assetPrefix: ghPages ? "/cra-k.github.io/" : "", // customize this value
+        // exportPathMap: function () {
+        //     return {
+        //         "/": { page: "/" },
+        //         "/about": { page: "/" },
+        //         "/home": { page: "/" },
+        //         "/formation": { page: "/formations/git-github" },
+        //         "/formations": { page: "/formations/git-github" },
+        //     };
+        // },
+        // assetPrefix: !debug ? "/cra-web-page/" : "",
+        // webpack(config, { dev }) {
+        //     // Perform customizations to webpack config
+        //     config.module.rules = config.module.rules.map((rule) => {
+        //         if (rule.loader === "babel-loader") {
+        //             rule.options.cacheDirectory = false;
+        //         }
+        //         return rule;
+        //     });
+        //     // Important: return the modified config
+        //     return config;
+        // },
     },
-};
+]);
