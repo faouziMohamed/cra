63a14003b00d79e73c2965a0c2b4919bc5ec285d
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-02-20 00:04:14 +0100

    Delete files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { enableThemes } from "./themes.js";
-import { displayModalContent } from "./modals.js";
-import { executeBody } from "./body.js";
-
-export default function main() {
-    enableThemes();
-    displayModalContent();
-    executeBody();
-}
