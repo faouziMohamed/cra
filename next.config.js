f012042e7355de848699e1b2fec30acb71cb9337
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-02-19 02:25:54 +0100

    Add new download link

--- next.config.js
+++ next.config.js
@@ -3,39 +3,44 @@ const optimizedImages = require("next-optimized-images");
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = withPlugins([
-    [optimizedImages, {}],
+  [optimizedImages, {}],
 
-    {
-        async redirects() {
-            return [
-                {
-                    source: "/about",
-                    destination: "/home",
-                    permanent: false,
-                },
-            ];
+  {
+    async redirects() {
+      return [
+        {
+          source: "/about",
+          destination: "/home",
+          permanent: false,
         },
-        async rewrites() {
-            return [
-                {
-                    source: "/home",
-                    destination: "/",
-                },
-                {
-                    source: "/formations",
-                    destination: "/formations/git-github",
-                },
-                {
-                    source: "/formation",
-                    destination: "/formations/git-github",
-                },
-                {
-                    source: "/dw/f/git-github",
-                    destination:
-                        "https://1drv.ms/p/s!Am4xVXV1LqvLrnACo-IRGQDOHx_7?e=4AVfID",
-                },
-            ];
-        },
-        assetPrefix: ghPages ? "/cra-k.github.io/" : "",
+      ];
     },
+    async rewrites() {
+      return [
+        {
+          source: "/home",
+          destination: "/",
+        },
+        {
+          source: "/formations",
+          destination: "/formations/git-github",
+        },
+        {
+          source: "/formation",
+          destination: "/formations/git-github",
+        },
+        {
+          source: "/dw/f/git-github",
+          destination:
+            "https://1drv.ms/p/s!Am4xVXV1LqvLrnACo-IRGQDOHx_7?e=4AVfID",
+        },
+        {
+          source: "/dw/f/powerpoint",
+          destination:
+            "https://1drv.ms/p/s!Am4xVXV1LqvLomzt5Z_a0slnIQGj?e=2t5mOZ",
+        },
+      ];
+    },
+    assetPrefix: ghPages ? "/cra-k.github.io/" : "",
+  },
 ]);
