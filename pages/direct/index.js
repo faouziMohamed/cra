fa9c6d3a5a8c9aa658be90baed11cceca54e1fbc
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-02-21 14:56:35 +0100

    Add todo

--- pages/direct/index.js
+++ pages/direct/index.js
@@ -30,6 +30,12 @@ export default function Direct() {
         <div
           className={`${css.article_wraper} flex`}
           style={{ width: "100%", height: "100%" }}>
+          {/* TODO:
+           * Make the page scrollable, Add option to make the direct private,
+           * Add style configuration on css module for the webPage
+           * Set the best size for embedded screen
+           * make the site lite
+           */}
           <iframe
             allow="camera; microphone; fullscreen; display-capture"
             src="https://meet.jit.si/git-github"
