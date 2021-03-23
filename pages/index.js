d66151611c4c2884cc71472e9f5d68f007910733
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-03-23 01:16:46 +0100

    Add style for covid 19 banner

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect } from "react";
 
-import ArticleLayout, {
-  PageContentData,
-  useThemes,
-} from "../components/articleLayout";
+import { PageContentData, useThemes } from "../components/articleLayout";
 // import BottomNavigation from "../components/bottomNavigation";
 import Carousel from "../components/Carousel";
 import Footer from "../components/footer";
@@ -13,9 +9,7 @@ import HeadData from "../components/HeadData";
 import Header from "../components/header";
 import { PageContent } from "../components/mainLayout";
 import Noscript from "../components/Noscript";
-import PageHeader from "../components/PageHeader";
 import { getHomePageData } from "../lib/posts";
-import css from "./404.module.css";
 import style from "./index.module.css";
 
 export function getStaticProps() {
@@ -38,76 +32,20 @@ export default function LandPage({ metadata, data, navigation }) {
     <>
       <Head>
         <title>{metadata.title}</title>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="/css/index.carousel.module.css"
+        />
       </Head>
       <HeadData metadata={metadata} />
       <Header metadata={metadata} />
       <div className="root" id="app">
-        <main className={`${css.main_wrapper} app-content main-content flex`}>
-          <div
-            className={`content-wrapper ${style.content_wrapper_has_carousel} `}>
+        <main className={`app-content main-content flex`}>
+          <div className={`${style.home_content_wrapper} `}>
             <span id="top"></span>
-            <div className={`${style.landpage_row_1} landpage_row_1`}>
-              <div className={`${style.row_1_container}`}>
-                <Carousel />
-                <section className={`${style.row_1_data}`}>
-                  <header className={`${style.main_header}`}>
-                    <h1 className={`${style.main_title}`}>
-                      {metadata.pageTitle}
-                    </h1>
-                    <p className={`${style.main_sub_title}`}>
-                      {metadata.pageSubtitle}
-                    </p>
-                  </header>
-                  {/* <PageHeader titles={metadata} /> */}
-                  <PageContent>
-                    {/* <PageContentData data={data} /> */}
-                    <section className={`${style.home_top_section}`}>
-                      <p className={`${style.home_top_content}`}>
-                        Le Club de Recherche Académique (CRA) est fondé en 2019
-                        par l’ACEM section de Kénitra. Ce club a pour but
-                        premier d’apporter de l’aide dans les études aux
-                        étudiants, stagiaires et employés comorien·ne·s qui se
-                        trouvent aux Maroc.
-                      </p>
-                      <div className={`${style.home_top_links_container}`}>
-                        <ul className={`${style.home_top_links_list}`}>
-                          <li className={`${style.home_top_link_item}`}>
-                            <Link href="/formations">
-                              <a className={`${style.home_top_link}`}>
-                                Formations
-                              </a>
-                            </Link>
-                          </li>
-                          <li className={`${style.home_top_link_item}`}>
-                            <Link href="/formations">
-                              <a className={`${style.home_top_link}`}>
-                                Webinaires
-                              </a>
-                            </Link>
-                          </li>
-                        </ul>
-                      </div>
-                    </section>
-                  </PageContent>
-                </section>
-              </div>
-            </div>
-
-            <section
-              className="alerte-section"
-              style={{ backgroundColor: "red" }}>
-              <h2>Protegez-vous contre le Covid </h2>
-            </section>
-            <article className="main-article flex">
-              <PageContent>
-                <PageContentData data={data} />
-              </PageContent>
-              {/* <BottomNavigation navigation={navigation} isArticle={isArticle} /> */}
-              <Footer />
-              <a className="to-top" alt="Monter en haut de la page" href="#top">
-                {""}
-              </a>
-            </article>
+            <HomePageRow1 metadata={metadata} />
+            <HomePageRow2 data={data} />
           </div>
         </main>
         <Noscript />
@@ -116,10 +54,122 @@ export default function LandPage({ metadata, data, navigation }) {
   );
 }
 
-export function Home({ metadata, data, navigation }) {
+function HomePageRow1({ metadata }) {
   return (
-    <>
-      <ArticleLayout metadata={metadata} data={data} navigation={navigation} />
-    </>
+    <div className={`${style.landpage_row_1} landpage_row_1`}>
+      <div className={`${style.row_1_container}`}>
+        <Carousel />
+        <section className={`${style.row_1_data}`}>
+          <header className={`${style.main_header}`}>
+            <h1 className={`${style.main_title}`}>{metadata.pageTitle}</h1>
+            <p className={`${style.main_sub_title}`}>{metadata.pageSubtitle}</p>
+          </header>
+          <section className={`${style.home_top_section}`}>
+            <p className={`${style.home_top_content}`}>
+              Recherche Académique (CRA) est fondé en 2019 par l’ACEM section de
+              Kénitra. Il a pour but premier d’apporter de l’aide dans les
+              études aux étudiants, stagiaires et employés comorien·ne·s qui se
+              trouvent aux Maroc.
+            </p>
+            <div className={`${style.home_top_links_container}`}>
+              <ul className={`${style.home_top_links_list}`}>
+                <li className={`${style.home_top_link_item}`}>
+                  <Link href="/formations">
+                    <a className={`${style.home_top_link}`}>Formations</a>
+                  </Link>
+                </li>
+                <li className={`${style.home_top_link_item}`}>
+                  <Link href="/formations">
+                    <a className={`${style.home_top_link}`}>Webinaires</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </section>
+      </div>
+    </div>
   );
 }
+
+function HomePageRow2({ data }) {
+  return (
+    <div className={`${style.landpage_row_2} landpage_row_2`}>
+      <div className={`${style.landpage_row_2_content_wrapper}`}>
+        <CovidAlerte />
+        <article className="main-article flex">
+          <PageContent>
+            <PageContentData data={data} />
+          </PageContent>
+          {/* <BottomNavigation navigation={navigation} isArticle={isArticle} /> */}
+          <Footer />
+          <article
+            className="to-top"
+            alt="Monter en haut de la page"
+            href="#top">
+            {""}
+          </article>
+        </article>
+      </div>
+    </div>
+  );
+}
+
+function CovidAlerte() {
+  return (
+    <section className={`${style.covid_alerte_section}`}>
+      <h2 className={`${style.covid_alerte_title}`}>
+        <i
+          className={`fad fa-virus-slash ${style.covid_alerte_title_icon}`}></i>
+        <span className={`${style.covid_alerte_title_text}`}>
+          Protegez-vous contre le Corona Virus (Covid 19), mettez un masque,
+          sauvez des vies
+        </span>
+      </h2>
+      <div className={`${style.covid_alerte_list_item_container}`}>
+        <div className={`${style.covid_alerte_list_item_container_pos}`}>
+          <ul className={`${style.covid_alerte_list_item}`}>
+            <li className={`${style.covid_alerte_item}`}>
+              <i
+                className={`fad fa-head-side-mask ${style.covid_alerte_item_text}`}></i>
+              <span className={`${style.covid_alerte_item_text}`}>
+                Mettez un masque
+              </span>
+            </li>
+            <li className={`${style.covid_alerte_item}`}>
+              <i
+                className={`fad fa-hands-wash ${style.covid_alerte_item_text}`}></i>
+              <span className={`${style.covid_alerte_item_text}`}>
+                Lavez régulièrement vos mains
+              </span>
+            </li>
+            <li className={`${style.covid_alerte_item}`}>
+              <i
+                className={`fad fa-people-arrows ${style.covid_alerte_item_text}`}></i>
+              <span className={`${style.covid_alerte_item_text}`}>
+                Garder une distance de sécurité
+              </span>
+            </li>
+          </ul>
+          <a
+            href="https://www.who.int/fr/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub"
+            className={`${style.covid_more_info_btn}`}>
+            <i
+              className={`fas fa-info-circle ${style.covid_more_info_btn_icon}`}></i>
+            <span className={`${style.covid_more_info_btn_text}`}>
+              Plus d&apos; informations
+            </span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+// export function Home({ metadata, data, navigation }) {
+//   return (
+//     <>
+//       <ArticleLayout metadata={metadata} data={data} navigation={navigation} />
+//     </>
+//   );
+// }
